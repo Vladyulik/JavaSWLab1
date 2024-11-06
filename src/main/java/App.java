8fa4b821dc3fc117dab7014376df5e5d05f04582
vladyulik <vanya123wow321@gmail.com>
2024-11-06 18:46:27 +0200

    Add initial matrices

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,15 @@
 public class App {
     public static void main(String[] args) {
-        //
-        //
-        //
+        final short[][] MATRIX_A = {
+            {11, 255, -678},
+            {-2, 441, 0},
+            {54, -111, 512}
+        };
+
+        final short[][] MATRIX_B = {
+            {-173, 123, -154},
+            {0, 42, 455},
+            {-256, -143, 300}
+        };
     }
 }
